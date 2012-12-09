49ae63fc9f428ca77e283cb3ee718a6de20f82e4
Phil Callister <philcallister@gmail.com>
2012-12-08 20:22:06 -0600

    Initial commit: Clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsElasticTire::Application.config.secret_token = '505af7e7338e1c515b5a35d401e23df8000d5ef05ffbc3615f69b67c6067fac6725b6af97f0a1252f210529360571fbbcb5e91556b8f8d7835faf5dee0167f96'
