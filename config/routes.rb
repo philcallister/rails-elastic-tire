63669c03de7f5df23d1df5991a02e0b22e0081d3
Phil Callister <philcallister@gmail.com>
2012-12-08 20:23:12 -0600

    Added Tire support into the frontend of application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 RailsElasticTire::Application.routes.draw do
   root :to => 'articles#index'
 
-  resources :articles
+  resources :articles do
+    collection { get :search }
+  end
+
 
 
   # The priority is based upon order of creation:
